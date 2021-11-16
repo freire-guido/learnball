c9d6fae295189f1373530a04ea2c00ba6211ebc1
freire-guido <freireguido@outlook.com>
2021-11-16 16:27:19 -0300

    function objects to classes

--- lib.js
+++ lib.js
@@ -18,48 +18,50 @@ class Graph {
   }
 }
 
-function Player(x, y, team, r = 25) {
-  this.t = team;
-  this.r = r;
-  this.x = x;
-  this.y = y;
-  this.s = 0;
-  this.kick = false;
-  this.up = (f) => {
+class Player {
+  constructor(x, y, team, r = 25) {
+    this.t = team;
+    this.r = r;
+    this.x = x;
+    this.y = y;
+    this.s = 0;
+    this.kick = false;
+  }
+  up = (f) => {
     this.y += f;
   }
-  this.side = (s) => {
+  side = (s) => {
     this.x += s;
   }
-  this.render = (index = undefined) => {
+  render = (index = undefined) => {
     if (this.t == 1) {
       fill(0, 0, 255);
     }
     else {
-      fill(255, 0, 0)
+      fill(255, 0, 0);
     }
     ellipse(this.x, this.y, this.r * 2);
     if (index !== undefined) {
       textSize(20);
       textAlign(CENTER);
-      fill(255,255,255);
+      fill(255, 255, 255);
       text(index, this.x, this.y + (this.r - 20) / 2);
     }
   }
-  this.inputs = (players, ball, goal) => {
+  inputs = (players, ball, goal) => {
     let inputs = [];
     let dist = [];
     if (this.t == 0) {
-      players.slice(players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))) });
+      players.slice(players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))); });
       inputs.push(players[dist.indexOf([...dist].sort()[0]) + players.length / 2].x - this.x, players[dist.indexOf([...dist].sort()[0]) + players.length / 2].y - this.y);
       dist = [];
-      players.slice(0, players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))) });
+      players.slice(0, players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))); });
       inputs.push(players[dist.indexOf([...dist].sort()[1])].x - this.x, players[dist.indexOf([...dist].sort()[1])].y - this.y);
     } else {
-      players.slice(0, players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))) });
+      players.slice(0, players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))); });
       inputs.push(players[dist.indexOf([...dist].sort()[0])].x - this.x, players[dist.indexOf([...dist].sort()[0])].y - this.y);
       dist = [];
-      players.slice(players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))) });
+      players.slice(players.length / 2).forEach(player => { dist.push(sqrt(sq(this.x - player.x) + sq(this.y - player.y))); });
       inputs.push(players[dist.indexOf([...dist].sort()[1]) + players.length / 2].x - this.x, players[dist.indexOf([...dist].sort()[1]) + players.length / 2].y - this.y);
     }
     inputs.push(goal.x - this.x, goal.y - this.y);
@@ -68,11 +70,13 @@ function Player(x, y, team, r = 25) {
   }
 }
 
-function Ball(x, y, r = 10) {
-  this.r = r;
-  this.x = x;
-  this.y = y;
-  this.collision = (dx, dy, kick) => {
+class Ball {
+  constructor(x, y, r = 10) {
+    this.r = r;
+    this.x = x;
+    this.y = y;
+  }
+  collision = (dx, dy, kick) => {
     if (kick) {
       this.x -= dx * 4;
       this.y -= dy * 4;
@@ -82,7 +86,7 @@ function Ball(x, y, r = 10) {
       this.y -= dy;
     }
   }
-  this.render = () => {
+  render = () => {
     fill(0, 255, 0);
     ellipse(this.x, this.y, this.r * 2);
   }
