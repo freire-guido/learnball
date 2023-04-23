5197c7dc8c73ae4174aade87be295489a36ea2e4
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-04-23 14:39:51 -0300

    getStateTensor undefine param

--- football.js
+++ football.js
@@ -13,12 +13,17 @@ export class Football {
         this.players = tf.randomUniform([2, this.teamSize + 1], this.pitchWidth - this.pitchWidth / 2, this.pitchWidth + this.pitchWidth / 2);
         this.ball = tf.randomUniform([2, 1], this.pitchWidth - this.pitchWidth / 2, this.pitchWidth + this.pitchWidth / 2);
     }
-    getStateTensor(skip = this.teamSize) {
-        return this.players.slice([0, 0], [2, skip]).concat([this.players.slice([0, skip + 1]), this.ball], 1);
+    getStateTensor(skip = undefined) {
+        if (skip !== undefined) {
+            return this.players.slice([0, 0], [2, skip]).concat([this.players.slice([0, skip + 1]), this.ball], 1);
+        }
+        else {
+            return this.players.concat([this.ball], 1);
+        }
     }
     update(actions) {
+        this.players = this.players.add(actions.mul(this.epsilon));
         return tf.tidy(() => {
-            this.players = this.players.add(actions.mul(this.epsilon));
             const dplayer = tf.sub(this.players, this.ball);
             const collisions = this.booleanMask(dplayer, dplayer.euclideanNorm(0).less(this.playerSize, this.ballSize), 1);
             if (collisions.shape[1] != 0) {
@@ -29,7 +34,7 @@ export class Football {
     }
     isDone() {
         //missing opponent goal and out of field conditions
-        return tf.sub(this.pitchWidth, this.ball.slice([0, 0], [1, 1])).less(this.playerSize) && tf.sub(this.pitchHeight * 0.5, this.ball.slice([1, 0])).abs().less(this.goalWidth);
+        return tf.sub(this.pitchWidth, this.ball.slice([0, 0], [1, 1])).less(this.playerSize) && tf.sub(this.pitchHeight * 0.5, this.ball.slice([1, 0])).abs().less(this.goalWidth).dataSync()[0];
     }
     booleanMask(tensor, mask, axis = 0) {
         const indices = [];
